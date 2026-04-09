32e6eefa4df2dfb1fd3127caf5223350deb4b977
hljubic <hrvoje.ljubic@gmail.com>
2026-04-09 16:42:28 +0200

    Novi modeli i rute

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from extensions import db, migrate
-from models import Profesor, Kolegij
+from models import Profesor, Kolegij, Ucionica
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:csdigital@localhost/aup1"
@@ -8,19 +8,40 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:csdigital@localhos
 db.init_app(app)
 migrate.init_app(app, db)
 
+
 # mozemo root rutu
 @app.route('/')
 def index():
     return "početna"
 
-@app.route('/novi_kolegij/<naziv>/<ects>') # to su dva required polja
-def novi_kolegij(naziv, ects):
 
+@app.route('/novi_kolegij/<naziv>/<ects>')  # to su dva required polja
+def novi_kolegij(naziv, ects):
     kolegij = Kolegij(naziv=naziv, ects=ects)
     db.session.add(kolegij)
     db.session.commit()
 
     return "dodano"
+
+
+@app.route('/nova_ucionica/<oznaka>/<kat>/<kapacitet>')
+def nova_ucionica(oznaka, kat, kapacitet):
+    ucionica = Ucionica(oznaka=oznaka, kat=kat, kapacitet=kapacitet)
+
+    db.session.add(ucionica)
+    db.session.commit()
+
+    return "Dodana učionica."
+
+@app.route('/ucionice')
+def ucionice():
+    ucionice = Ucionica.query.all()
+
+    return jsonify([u.to_dict() for u in ucionice])
+
+# 4. zadatak - app.py
+# 4.1. napraviti rutu za dodavanje nove učionice (primjer novi_kolegij)
+# 4.2. napraviti rutu za ispis svih učionica.
 
 # Samostalno napraviti rutu za ispis svih kolegija.
 # ovo je najosnovniji primjer, a primjere bolje prakse ćemo proći na idućim vježbama.
--- models.py
+++ models.py
@@ -26,3 +26,41 @@ class Kolegij(db.Model):
 
     nositelj_id = db.Column(db.Integer, db.ForeignKey("profesori.id"), nullable=True)
 
+class Ucionica(db.Model):
+    __tablename__ = "ucionice"
+
+    id = db.Column(db.Integer, primary_key=True)
+    oznaka = db.Column(db.String(80), nullable=False)
+    kat = db.Column(db.Integer, nullable=True)
+    kapacitet = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "oznaka": self.oznaka,
+            "kapacitet": self.kapacitet
+        }
+
+class TerminNastave(db.Model):
+    __tablename__ = "termini_nastave"
+
+    id = db.Column(db.Integer, primary_key=True)
+    dan_u_tjednu = db.Column(db.String(10), nullable=False)
+    vrijeme_pocetka = db.Column(db.String(10), nullable=False)
+    trajanje = db.Column(db.Integer, nullable=False)
+
+    profesor_id = db.Column(db.Integer, db.ForeignKey("profesori.id"), nullable=True)
+    kolegij_id = db.Column(db.Integer, db.ForeignKey("kolegiji.id"), nullable=True)
+
+
+# U terminalu pokrenemo dvije naredbe:
+
+# flask db migrate -m "napravljena tablica ucionice"
+# flask db upgrade
+
+# 3. zadatak
+# napraviti klasu TerminNastave (tablice termini_nastave)
+# dodati stupce id, dan_u_tjednu (string), vrijeme_pocetka (string), trajanje (integer)
+# dodati strane ključeve na profesor_id i kolegij_id
+
+
