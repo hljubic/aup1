4953a737ee5352220a592e3c3bde2c536076caac
hljubic <hrvoje.ljubic@gmail.com>
2026-05-07 15:14:56 +0200

    Promjene danas

--- aup-frontend/src/views/ONama.vue
+++ aup-frontend/src/views/ONama.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
+import {reactive} from "vue";
 
+const profesor = reactive({
+  ime: '',
+  prezime: '',
+  email: '',
+  titula: ''
+})
+
+async function spremiProfesora() {
+  const response = await fetch('http://localhost:5005/profesori', {
+    method: 'POST',
+     headers: {
+     'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(profesor)
+  })
+
+  const data = await response.json()
+}
 </script>
 
 <template>
-Stranica "O nama"
+  <v-text-field v-model="profesor.ime"
+                label="Ime.."></v-text-field>
+  <v-text-field v-model="profesor.prezime"
+                label="Prezime.."></v-text-field>
+  <v-text-field v-model="profesor.email"
+                label="E-pošta.."></v-text-field>
+  <v-text-field v-model="profesor.titula"
+                label="Titula.."></v-text-field>
+
+  <v-btn text="Spremi" @click="spremiProfesora"></v-btn>
 </template>
 
 <style scoped>
--- aup-frontend/src/views/Pocetna.vue
+++ aup-frontend/src/views/Pocetna.vue
@@ -1,31 +1,124 @@
 <script setup lang="ts">
-import {ref, onMounted} from "vue";
+import {ref, onMounted, reactive} from "vue";
 
+const profesor = reactive({
+  ime: '',
+  prezime: '',
+  email: '',
+  titula: ''
+})
+
+const loading = ref(false)
 const fakultet = ref("Test");
+const prikazi_dialog = ref(false);
 const profesori = ref([]);
 const headers = [
   {title: 'ID', value: 'id'},
   {title: 'E-pošta', value: 'email'},
   {title: 'Ime', value: 'ime'},
   {title: 'Prezime', value: 'prezime'},
+  { title: 'Akcije', key: 'actions', sortable: false },
 ]
 
 async function dohvatiProfesore() {
+  loading.value = true
   const response = await fetch('http://localhost:5005/profesori')
   const data = await response.json()
+  loading.value = false
 
   profesori.value = data
+}
+
+async function urediProfesora() {
+  loading.value = true
+  const response = await fetch(`http://localhost:5005/profesori/${profesor.id}`, {
+    method: 'PUT',
+     headers: {
+     'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(profesor)
+  })
+
+  //const data = await response.json()
+
+  loading.value = false
+  dohvatiProfesore()
+  prikazi_dialog.value = false
 }
 
 onMounted(async () => {
   // await dohvatiProfesore()
 })
 
+function pregledaj(profesor: any) {
+  console.log('Pregledaj', profesor)
+}
+
+function uredi(odabrani_profesor: any) {
+  prikazi_dialog.value = true
+  Object.assign(profesor, odabrani_profesor)
+  console.log('Uredi', profesor)
+}
+
+async function obrisi(profesor: any) {
+  const response = await fetch(`http://localhost:5005/profesori/${profesor.id}`, {
+    method: 'DELETE',
+  })
+
+  await dohvatiProfesore()
+}
 
 </script>
 
 <template>
   Početna stranica. <br>
+
+  <v-dialog v-model="prikazi_dialog" max-width="500">
+    <v-card>
+      <v-card-title>
+        Uredi profesora
+      </v-card-title>
+
+      <v-card-text>
+        <v-text-field
+          v-model="profesor.ime"
+          label="Ime"
+        />
+
+        <v-text-field
+          v-model="profesor.prezime"
+          label="Prezime"
+        />
+
+        <v-text-field
+          v-model="profesor.email"
+          label="E-pošta"
+        />
+
+        <v-text-field
+          v-model="profesor.titula"
+          label="Titula"
+        />
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer />
+
+        <v-btn
+          text="Odustani"
+          variant="text"
+          @click="prikazi_dialog = false"
+        />
+
+        <v-btn
+          text="Spremi"
+          color="primary"
+          :loading="loading"
+          @click="urediProfesora"
+        />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 
   <v-text-field
       hint="Ovo je hint"
@@ -45,7 +138,48 @@ onMounted(async () => {
 
   <br>
 
-  <v-data-table :headers="headers" :items="profesori"></v-data-table>
+  <v-data-table :loading="loading" :headers="headers" :items="profesori">
+     <template #item.actions="{ item }">
+
+
+      <v-tooltip text="Pregledaj">
+        <template #activator="{ props }">
+          <v-btn
+            v-bind="props"
+            icon="mdi-eye"
+            variant="text"
+            size="small"
+            @click="pregledaj(item)"
+          />
+        </template>
+      </v-tooltip>
+
+      <v-tooltip text="Uredi">
+        <template #activator="{ props }">
+          <v-btn
+            v-bind="props"
+            icon="mdi-pencil"
+            variant="text"
+            size="small"
+            @click="uredi(item)"
+          />
+        </template>
+      </v-tooltip>
+
+      <v-tooltip text="Obriši">
+        <template #activator="{ props }">
+          <v-btn
+            v-bind="props"
+            icon="mdi-delete"
+            variant="text"
+            size="small"
+            @click="obrisi(item)"
+          />
+        </template>
+      </v-tooltip>
+
+    </template>
+  </v-data-table>
 
   <br>
 
